cdf6c74a144a22cafd1c031c7b798ac250a23ea2
slalom0914 <slalom0914@gmail.com>
2025-05-08 12:44:11 +0900

    fix

--- 02.해커뉴스/news-step1/app.js
+++ 02.해커뉴스/news-step1/app.js
@@ -3,19 +3,27 @@ const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json"
 xhr.open("GET", NEWS_URL, false)
 xhr.send() //waitting 상황 기다리는 중
 
-const newsList = JSON.parse(xhr.responseText)
+const newsList = JSON.parse(xhr.responseText)//NodeList
 console.log(newsList);
-console.log(typeof newsList);//object출력됨 {}, []
-//배열은 어떻게 선언하나요?
-const arr = []
-arr = newsList //치환
-console.log(newsList.length);
-console.log(arr.length);//원소의 갯수가 30개인가?
-console.log(arr[0]);
+console.log(newsList[0]);
+console.log(newsList[1]);
+console.log(newsList[1].id);
+//ul태그는 부모태그이므로 딱 한번만 감싸주면 된다.
+//그래서 for문 밖에 써야 합니다.
+const ul = document.createElement("ul")
+//for(let i=0;i<10;i=i+1){
+for(let i=1;i<=10;i=i+1){
+  //li태그는 10번 반복되어야 하니까 for문 안에 써야 한다. 위치문제
+  const li = document.createElement("li")
+  li.innerText = "뉴스제목ㄴㅇㄹㅇㄴㄹㅇㄴㄹㅇㄴ"
+  //appendChild - how?
+  //ul.appendChild(document.createElement("li"))
+  ul.appendChild(li) //ul태그 안에 li태그 추가를 10번 반복해줘
+}
 
-const v_root = document.querySelector("#root")
+const v_root = document.querySelector("#root").appendChild(ul)
 //console.log(v_root.innerHTML);
-v_root.innerHTML = "조회결과"
+//v_root.innerHTML = "조회결과"
 /*
 
 */
--- 03.자바스크립트/day2/object1.js
+++ 03.자바스크립트/day2/object1.js
@@ -10,6 +10,16 @@ const userB = {
   gender: "남자"
 }
 
+const users = [userA, userB]
+console.log(users[0]);
+console.log(users[0].age);
+console.log(users[0]["age"]);
+console.log(users[0].gender);
+console.log(users[0]["gender"]);
+console.log(users[1].name);
+console.log(users[1]["name"]);
+console.log(users[1]);
+
 console.log(userA);
 console.log(userA.name + " , " +userB.name);
 console.log(userA.age);
