97f31bae15ae812d59ede4563cc0858d30ed9761
slalom0914 <slalom0914@gmail.com>
2025-05-08 12:54:39 +0900

    해커뉴스1-2

--- 02.해커뉴스/news-step1/app4.js
+++ 02.해커뉴스/news-step1/app4.js
@@ -8,10 +8,16 @@ console.log(newsList);
 console.log(newsList[0]);
 console.log(newsList[1]);
 console.log(newsList[1].id);
+//ul태그는 부모태그이므로 딱 한번만 감싸주면 된다.
+//그래서 for문 밖에 써야 합니다.
 const ul = document.createElement("ul")
+//for(let i=0;i<10;i=i+1){
 for(let i=1;i<=10;i=i+1){
+  //li태그는 10번 반복되어야 하니까 for문 안에 써야 한다. 위치문제
   const li = document.createElement("li")
-  li.innerText = newsList[i].title
+  li.innerText = "뉴스제목ㄴㅇㄹㅇㄴㄹㅇㄴㄹㅇㄴ"
+  //appendChild - how?
+  //ul.appendChild(document.createElement("li"))
   ul.appendChild(li) //ul태그 안에 li태그 추가를 10번 반복해줘
 }
 
