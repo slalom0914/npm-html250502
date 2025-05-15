debd85032950187bd6abd800cc82a364fc135dea
slalom0914 <slalom0914@gmail.com>
2025-05-15 18:17:30 +0900

    로그인디자인추가

--- 05.디자인/레이아웃/login/bookApp/firebase.js
+++ 05.디자인/레이아웃/login/bookApp/firebase.js
@@ -10,6 +10,6 @@
     measurementId: "G-JTVQT3FC92"
   };
   //파이어베이스 API를 활용하여 웹서비스를 제공받기 위한 초기화 작업임.
-  //앞에 export를 붙여야 외부(다른파일 예:html, js)에서 사용이 가능하다.(ES6 모듈)
+  //변수 앞에 export를 붙여야 외부(다른파일 예:html, js)에서 사용이 가능하다.(ES6 모듈)
   //초기화 - 인증 - 서버측 요청 - 응답 (토큰값 쥔다)
   export const app = initializeApp(firebaseConfig);
