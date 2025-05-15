64fd735c45a6e23aca195e8f03f14cefc3855c1e
slalom0914 <slalom0914@gmail.com>
2025-05-15 16:18:31 +0900

    파이어베이스환경설정

---
+++ 05.디자인/레이아웃/login/bookApp/firebase.js
@@ -0,0 +1,15 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js";
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FS_APIKEY,
+    authDomain: process.env.REACT_APP_FS_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_FS_DATABASEURL,
+    projectId: process.env.REACT_APP_FS_PROJECTID,
+    storageBucket: process.env.REACT_APP_FS_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FS_MESSAGINGSenderId,
+    appId: process.env.REACT_APP_FS_APPID,
+    measurementId: "G-JTVQT3FC92"
+  };
+  //파이어베이스 API를 활용하여 웹서비스를 제공받기 위한 초기화 작업임.
+  //앞에 export를 붙여야 외부(다른파일 예:html, js)에서 사용이 가능하다.(ES6 모듈)
+  //초기화 - 인증 - 서버측 요청 - 응답 (토큰값 쥔다)
+  export const app = initializeApp(firebaseConfig);
